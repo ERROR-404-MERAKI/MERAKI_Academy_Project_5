a4f401c5fcedb04c3ef90a07ef0b818cac61e705
HasanTorman <torman.hassan@gmail.com>
2022-06-01 22:47:10 +0300

    get all posts And by Id

--- backend/routes/post.js
+++ backend/routes/post.js
@@ -1,11 +1,21 @@
 const express = require("express");
 
-const { createNewPost } = require("../controllers/post");
+const {
+  createNewPost,
+  getAllPost,
+  getPostById,
+} = require("../controllers/post");
 const { authentication } = require("../middlewares/authentication");
 
 const postRouter = express.Router();
 
-// endpoint /login
+//POST=> endpoint /post
 postRouter.post("/", authentication, createNewPost);
+
+//GET => endpoint /post
+postRouter.get("/", getAllPost);
+
+//GET => endpoint /post/id
+postRouter.get("/:id", getPostById);
 
 module.exports = postRouter;
