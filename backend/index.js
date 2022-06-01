218810af6025d4e89270f906e607bd46ceb2c54b
MousaHussen <Mousahusseen@gmail.com>
2022-06-01 20:28:14 +0300

    update register and role

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,18 @@
 const express = require("express");
 require("dotenv").config();
-require("./models/db")
-
+require("./models/db");
 
 const app = express();
 //Router
 const roleRouter = require("./routes/role");
+const registerRouter = require("./routes/register");
 
 //built-in middleware
 app.use(express.json());
 app.use("/role", roleRouter);
+app.use("/register", registerRouter);
 
-
-
-//basic server 
+//basic server
 
 const PORT = process.env.PORT || 5000;
 
