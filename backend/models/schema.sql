9f902511929c8cf838aa3203c379f933ab4008e6
HasanTorman <torman.hassan@gmail.com>
2022-06-01 15:01:58 +0300

    edit sql database

--- backend/models/schema.sql
+++ backend/models/schema.sql
@@ -1,14 +1,9 @@
--- welcome to mtsql
+-- -- welcome to mtsql
+DROP DATABASE meraki_academy_project_5;
 
-CREATE DATABASE meraki_academy_project_5
-USE meraki_academy_project_5
+CREATE DATABASE meraki_academy_project_5;
 
-
--- welcome to mtsql
-
-
-
-
+USE meraki_academy_project_5;
 
 CREATE TABLE roles (
     id INT AUTO_INCREMENT NOT NULL,
@@ -21,13 +16,14 @@ CREATE TABLE users (
     id INT AUTO_INCREMENT NOT NULL,
     firstName VARCHAR(255) NOT NULL,
     lastName VARCHAR(255) NOT NULL,
-    age INT NUT NULL,
+    age INT NOT NULL,
     ProfilePicture VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
     is_deleted TINYINT DEFAULT 0,
-    FOREIGN KEY role_id REFERENCES roles(id),
-    PRIMARY KEY (id)
+    roleId INT,
+    FOREIGN KEY (roleId) REFERENCES roles(id),
+    PRIMARY KEY(id)
 );
 
 CREATE TABLE posts (
@@ -35,9 +31,9 @@ CREATE TABLE posts (
     description VARCHAR (255) NOT NULL,
     media VARCHAR(255) NOT NULL,
     date DATETIME DEFAULT 0,
-    like INT DEFAULT 0,
+    likes INT,
     is_deleted TINYINT DEFAULT 0,
-    PRIMARY KEY (id),
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE bookmarks (
@@ -45,8 +41,8 @@ CREATE TABLE bookmarks (
     post_id INT,
     user_id INT NOT NULL,
     is_deleted TINYINT DEFAULT 0,
-    FOREIGN KEY post_id REFERENCES posts (id),
-    FOREIGN KEY user_id REFERENCES users (id),
+    FOREIGN KEY (post_id) REFERENCES posts (id),
+    FOREIGN KEY (user_id) REFERENCES users (id),
     PRIMARY KEY (id)
 );
 
@@ -54,7 +50,7 @@ CREATE TABLE messages (
     id INT AUTO_INCREMENT NOT NULL,
     message VARCHAR(255) NOT NULL,
     user_id INT NOT NULL,
-    FOREIGN KEY user_id REFERENCES users(id),
+    FOREIGN KEY (user_id) REFERENCES users(id),
     is_deleted TINYINT DEFAULT 0,
     PRIMARY KEY (id)
 );
@@ -64,7 +60,7 @@ CREATE TABLE storys (
     story VARCHAR (255) NOT NULL,
     date DATETIME NOT NULL,
     user_id INT NOT NULL,
-    FOREIGN KEY user_id REFERENCES users(id),
+    FOREIGN KEY (user_id) REFERENCES users(id),
     is_deleted TINYINT DEFAULT 0,
     PRIMARY KEY (id)
 );
@@ -75,8 +71,8 @@ CREATE TABLE comments (
     date DATETIME,
     user_id INT NOT NULL,
     post_id INT,
-    FOREIGN KEY user_id REFERENCES users(id),
-    FOREIGN KEY post_id REFERENCES posts(id),
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (post_id) REFERENCES posts(id),
     is_deleted TINYINT DEFAULT 0,
     PRIMARY KEY (id)
 );
@@ -92,17 +88,17 @@ CREATE TABLE role_permissions (
     id INT AUTO_INCREMENT NOT NULL,
     role_id INT,
     permission_id INT,
-    FOREIGN KEY role_id REFERENCES roles(id),
-    FOREIGN KEY permission_id REFERENCES permissions(id),
+    FOREIGN KEY (role_id) REFERENCES roles(id),
+    FOREIGN KEY (permission_id) REFERENCES permissions(id),
     PRIMARY KEY (id)
 );
-CREATE TABLE follow  (
+
+CREATE TABLE follow (
     id INT AUTO_INCREMENT NOT NULL,
     user_id INT NOT NULL,
     person_id INT NOT NULL,
-    FOREIGN KEY user_id REFERENCES users(id),
-    FOREIGN KEY person_id REFERENCES users(id),
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (person_id) REFERENCES users(id),
     is_deleted TINYINT DEFAULT 0,
     PRIMARY KEY (id)
-
 );
