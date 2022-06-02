7d7dc230cf961a41ebe7c54b7d699587294e3477
areejassfour <areej.assfour@gmail.com>
2022-06-02 10:27:08 +0300

    git all users

--- backend/controllers/register.js
+++ backend/controllers/register.js
@@ -34,7 +34,24 @@ const register = async (req, res) => {
     });
   });
 };
-
+//GetAllUsers
+const getAllUser = (req, res) => {
+  const query = `SELECT * FROM users WHERE is_deleted=0;`;
+  connection.query(query, (err, result) => {
+    if (err) {
+     return res.status(500).json({
+        success: false,
+        massage: "server error",
+        err: err,
+      });
+    }
+    res.status(200).json({
+      success: true,
+      massage: "All the users",
+      result: result,
+    });
+  });
+};
 module.exports = {
-  register,
+  register,getAllUser
 };
