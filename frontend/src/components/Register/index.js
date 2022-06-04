0fefb52be3af58be6d8cddb37beb4a0765657a0b
MousaHussen <Mousahusseen@gmail.com>
2022-06-04 21:50:47 +0300

    update register for pic

--- frontend/src/components/Register/index.js
+++ frontend/src/components/Register/index.js
@@ -1,4 +1,4 @@
-import React, {  useState } from "react";
+import React, { useState } from "react";
 import "./style.css";
 import axios from "axios";
 
@@ -6,8 +6,6 @@ import { useSelector } from "react-redux";
 // =================================================================
 
 const Register = () => {
-  
-
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
   const [age, setAge] = useState(0);
@@ -17,97 +15,126 @@ const Register = () => {
   const roleId = "1";
   const [message, setMessage] = useState("");
   const [status, setStatus] = useState(false);
+  const [image, setImage] = useState("");
+  const [url, setUrl] = useState("");
 
-  const {isLoggedIn}= useSelector((state)=>{
+  const { isLoggedIn } = useSelector((state) => {
     return {
       isLoggedIn: state.auth.isLoggedIn,
     };
-  })
-  
+  });
 
   // =================================================================
 
-  const addNewUser = async (e) => {
-    e.preventDefault(); 
-    try {
-      const result = await axios.post("http://localhost:5000/register", {
+  const addUserToBackend = () => {
+    axios
+      .post("http://localhost:5000/register", {
         firstName,
         lastName,
-        age,
-        ProfilePicture,
         email,
         password,
         roleId,
+        age,
+        ProfilePicture: image,
+      })
+      .then((result) => {
+        setMessage(result.data.message);
+        //console.log(result);
+      })
+      .catch((err) => {
+        console.log(err.response.data.message);
+        setMessage(err.response.data.message);
       });
-      if (result.data.success) {
-        setStatus(true);
-        setMessage("The user has been created successfully");
-      } else throw Error;
-    } catch (error) {
-      setStatus(false);
-      if (error.response && error.response.data) {
-        return setMessage(error.response.data.message);
-      }
-      setMessage("Error happened while register, please try again");
-    }
+  };
+  //console.log(image);
+  const uploadImage = async () => {
+    const data = new FormData();
+    data.append("file", ProfilePicture);
+    data.append("upload_preset", "srcmongo");
+    data.append("cloud_name", "mousa");
+
+    fetch("https://api.cloudinary.com/v1_1/mousa/image/upload", {
+      method: "post",
+      body: data,
+    })
+      .then((resp) => resp.json())
+      .then((data) => {
+        console.log(data);
+        setUrl(data.url);
+        setImage(data.url);
+      })
+      .catch((err) => console.log(err));
   };
 
   // =================================================================
 
   return (
     <>
-      <div className="Form">
-        {!isLoggedIn ? (
-          <>
-            <p className="Title">Register:</p>
-            <form onSubmit={addNewUser}>
-              <br />
-              <input
-                type="text"
-                placeholder="First Name"
-                onChange={(e) => setFirstName(e.target.value)}
-              />
-              <br />
-              <input
-                type="text"
-                placeholder="Last Name"
-                onChange={(e) => setLastName(e.target.value)}
-              />
-              <br />
-              <input
-                type="number"
-                placeholder="Age"
-                onChange={(e) => setAge(e.target.value)}
-              />
-              <br />
-              <input
-                type="text"
-                placeholder="profile Pircture"
-                onChange={(e) => setProfilePicture(e.target.value)}
-              />
-              <br />
-              <input
-                type="email"
-                placeholder="Email"
-                onChange={(e) => setEmail(e.target.value)}
-              />
-              <br />
-              <input
-                type="password"
-                placeholder="Password"
-                onChange={(e) => setPassword(e.target.value)}
-              />
-              <br />
-              <button>Register</button>
-              <br />
-            </form>
-            {status
-              ? message && <div className="SuccessMessage">{message}</div>
-              : message && <div className="ErrorMessage">{message}</div>}
-          </>
-        ) : (
-          <p>Logout First</p>
-        )}
+      <div className="mainRejister">
+        <input
+          className="inputRegister"
+          placeholder="First Name .."
+          type="text"
+          onChange={(e) => {
+            setFirstName(e.target.value);
+          }}
+        ></input>{" "}
+        <br />
+        <input
+          className="inputRegister"
+          placeholder="Last Name .. "
+          type="text"
+          onChange={(e) => {
+            setLastName(e.target.value);
+          }}
+        ></input>{" "}
+        <br />
+        <input
+          className="inputRegister"
+          placeholder="Age.. "
+          type="number"
+          onChange={(e) => {
+            setAge(e.target.value);
+          }}
+        ></input>{" "}
+        <br />
+        <input
+          className="inputRegister"
+          placeholder="Email .. "
+          type="email"
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+        ></input>{" "}
+        <br />
+        <input
+          className="inputRegister"
+          placeholder="Password .."
+          type="password"
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+        ></input>{" "}
+        <br />
+        <div>
+          <input
+            className="inputRegister"
+            type="file"
+            onChange={(e) => {
+              // console.log(e.target.files[0]);
+              setProfilePicture(e.target.files[0]);
+            }}
+          ></input>{" "}
+          <button className="bb" onClick={uploadImage}>
+            <svg width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+              <path d="M4.406 1.342A5.53 5.53 0 0 1 8 0c2.69 0 4.923 2 5.166 4.579C14.758 4.804 16 6.137 16 7.773 16 9.569 14.502 11 12.687 11H10a.5.5 0 0 1 0-1h2.688C13.979 10 15 8.988 15 7.773c0-1.216-1.02-2.228-2.313-2.228h-.5v-.5C12.188 2.825 10.328 1 8 1a4.53 4.53 0 0 0-2.941 1.1c-.757.652-1.153 1.438-1.153 2.055v.448l-.445.049C2.064 4.805 1 5.952 1 7.318 1 8.785 2.23 10 3.781 10H6a.5.5 0 0 1 0 1H3.781C1.708 11 0 9.366 0 7.318c0-1.763 1.266-3.223 2.942-3.593.143-.863.698-1.723 1.464-2.383z" />
+              <path d="M7.646 4.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707V14.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708l3-3z" />
+            </svg>{" "}
+          </button>
+        </div>
+        <div>
+          <button onClick={addUserToBackend}>Register</button>
+        </div>
       </div>
     </>
   );
