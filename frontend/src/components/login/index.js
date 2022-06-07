bca2059d77f18a2c2db78ae2c5d10beb486bb09a
MousaHussen <Mousahusseen@gmail.com>
2022-06-08 01:08:32 +0300

    some edit on register

--- frontend/src/components/login/index.js
+++ frontend/src/components/login/index.js
@@ -33,21 +33,53 @@ const Login = () => {
       });
   };
   return (
-    <div className="login">
-      <p className="Title">Login:</p>
-      <input
-        type="email"
-        placeholder="Email"
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <input
-        type="password"
-        placeholder="Password"
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <button onClick={() => accessUser()}>Login</button>
+    <div className="container co_login">
+      <div className="login">
+        <div className="leftSide">
+          <img
+            className="imglog"
+            src="https://www.instagram.com/static/images/homepage/screenshots/screenshot4.png/a4fd825e3d49.png"
+          />
+        </div>
+        <div className="rightSide">
+          <div className="insideRight">
+          <div className="img_middle  zi">
+          <img
+            className="insta_img zi"
+            src="https://www.instagram.com/static/images/web/logged_out_wordmark.png/7a252de00b20.png"
+          />
+        </div>
+        <div className="zi inRegister">
+            <input className="inputRegister"
+              type="email"
+              placeholder="Email"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <input className="inputRegister"
+              type="password"
+              placeholder="Password"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <button className="inputRegister butto divpp logFb xi" onClick={() => accessUser()}>Login</button>
+            </div>
 
-      <div className="error">{message}</div>
+            <div className="messagelook">  {message}  </div>
+
+            <div className="threediv zi">
+          <div className="fir">_______</div>
+          <div className="sec">OR</div>
+          <div className="fir">_______</div>
+        </div>
+
+        <div className="divll">
+          <button className="logFB">log in with Facebook</button>
+        </div>
+        <div className="toLog xr"><p>Don't have an account?</p><button onClick={()=>{
+          history("/register")
+        }} className="chan">Sign up</button></div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
