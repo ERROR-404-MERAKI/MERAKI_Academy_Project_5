3a14a8f80cbf87416e904050e24bc24e8e08afd6
HasanTorman <torman.hassan@gmail.com>
2022-06-08 03:44:49 +0300

    create Route for bookmark (FE )

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,7 @@ import Register from "./components/Register";
 import Login from "./components/Login";
 import Home from "./components/Home";
 import Profile from "./components/Profile";
+import Bookmark from "./components/Bookmark";
 //===========================
 
 function App() {
@@ -18,6 +19,7 @@ function App() {
         <Route path="/" element={<Login />} />
         <Route path="/home" element={<Home />} />
         <Route path="/profile" element={<Profile />} />
+        <Route path="/profile/saved" element={<Bookmark />} />
 
 
       </Routes>
