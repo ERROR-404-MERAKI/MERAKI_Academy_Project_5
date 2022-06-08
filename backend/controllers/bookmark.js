4c590325ce94bfd24e0aeb6c4d4ca49ee819cb4f
HasanTorman <torman.hassan@gmail.com>
2022-06-08 20:40:54 +0300

    update function bookmark backend

--- backend/controllers/bookmark.js
+++ backend/controllers/bookmark.js
@@ -3,8 +3,8 @@ const connection = require("../models/db");
 const addBookmark = (req, res) => {
   const postId = req.params.id;
   const userId = req.token.userId;
-  const query = `INSERT INTO bookmarks (post_id ,user_id) VALUES (?,?)`;
-  const data = [postId, userId];
+  const query = `SELECT * FROM bookmarks WHERE post_id =?`;
+  const data = [postId];
 
   connection.query(query, data, (err, result) => {
     if (err) {
@@ -14,12 +14,43 @@ const addBookmark = (req, res) => {
         err: err,
       });
     }
+    if (result.length > 0) {
+      const query = `UPDATE bookmarks SET is_deleted=0 WHERE post_id =? `;
+      const data = [postId];
 
-    res.status(201).json({
-      success: true,
-      massage: "Success bookmark added",
-      result,
-    });
+      connection.query(query, data, (err, result) => {
+        if (err) {
+          return res.status(500).json({
+            success: false,
+            massage: "server error",
+            err: err,
+          });
+        }
+        return res.status(200).json({
+          success: true,
+          message: "bookmark updated",
+          posts: result,
+        });
+      });
+    } else {
+      const query1 = `INSERT INTO bookmarks (post_id ,user_id) VALUES (?,?)`;
+      const data = [postId, userId];
+
+      connection.query(query1, data, (err, result) => {
+        if (err) {
+          return res.status(500).json({
+            success: false,
+            massage: "server error",
+            err: err,
+          });
+        }
+        res.status(201).json({
+          success: true,
+          massage: "Success bookmark added",
+          result,
+        });
+      });
+    }
   });
 };
 
@@ -51,11 +82,10 @@ const allBookmark = (req, res) => {
 
 const deleteBookmark = (req, res) => {
   const postId = req.params.id;
-  const query = `UPDATE bookmarks SET is_deleted=1 WHERE post_id =? `;
+  const query = `SELECT * FROM bookmarks WHERE post_id =? AND is_deleted=0 `;
   const data = [postId];
 
   connection.query(query, data, (err, result) => {
-    console.log(result);
     if (err) {
       return res.status(500).json({
         success: false,
@@ -63,17 +93,37 @@ const deleteBookmark = (req, res) => {
         err,
       });
     }
-    if (result.changedRows > 0) {
-      return res.status(201).json({
-        success: true,
-        message: "bookmark Deleted",
-        posts: result,
+    if (result) {
+      const query = `UPDATE bookmarks SET is_deleted=1 WHERE post_id =?`;
+      const data = [postId];
+
+      connection.query(query, data, (err, result) => {
+        if (err) {
+          return res.status(500).json({
+            success: false,
+            message: "Server Error",
+            err,
+          });
+        }
+        if (result.changedRows > 0) {
+          res.status(200).json({
+            success: true,
+            message: "bookmark Deleted",
+            posts: result,
+          });
+        } else {
+          res.status(404).json({
+            success: false,
+            message: `The post with id ⇾ ${postId} is already deleted`,
+          });
+        }
+      });
+    } else {
+      res.status(404).json({
+        success: false,
+        message: `The post with id ⇾ ${postId} is not found`,
       });
     }
-    res.status(404).json({
-      success: false,
-      message: `The post with id ⇾ ${postId} is not found`,
-    });
   });
 };
 module.exports = {
