b4f337220e3220040c44af0ce6105e8ecd78cb32
MousaHussen <Mousahusseen@gmail.com>
2022-06-09 15:21:51 +0300

    change style and index for home

--- frontend/src/components/Home/index.js
+++ frontend/src/components/Home/index.js
@@ -27,17 +27,15 @@ const Home = () => {
   const [removeBook, setRemoveBook] = useState(0);
   const [showButton, setShowButton] = useState(false);
 
-  const { token, posts, storys, comments } = useSelector(
-    (state) => {
-      return {
-        token: state.auth.token,
-        isLoggedIn: state.auth.isLoggedIn,
-        posts: state.posts.posts,
-        storys: state.storys.storys,
-        comments: state.comments.commented,
-      };
-    }
-  );
+  const { token, posts, storys, comments } = useSelector((state) => {
+    return {
+      token: state.auth.token,
+      isLoggedIn: state.auth.isLoggedIn,
+      posts: state.posts.posts,
+      storys: state.storys.storys,
+      comments: state.comments.commented,
+    };
+  });
 
   let numberOfPage = localStorage.getItem("NOP") || 1;
 
@@ -209,7 +207,6 @@ const Home = () => {
         }
       )
       .then((result) => {
-        
         if (result) {
           dispatch(addBookmark(result.data));
           setShowBook(id);
@@ -225,8 +222,6 @@ const Home = () => {
     axios
       .delete(`http://localhost:5000/bookmark/${id}`)
       .then((result) => {
-        
-
         if (result) {
           dispatch(deleteBookmark(result.data));
           setRemoveBook(id);
@@ -237,7 +232,6 @@ const Home = () => {
         console.log(err);
       });
   };
-
 
   useEffect(() => {
     getAllStorys();
@@ -254,19 +248,30 @@ const Home = () => {
       </div>
       <div className="post_home">
         <div className="left_side">
-          <h1>hello from dev left</h1>
+          
         </div>
         <div className="middle_side">
           <div className="storys-department">
             <div className="main_story">
               <div>
                 <button
-                  className="bb"
+                  className="bb set"
                   onClick={() => {
                     backStory();
                   }}
                 >
-                  back
+                  {" "}
+                  <svg
+                    className="svg"
+                    width="20"
+                    height="20"
+                    viewBox="0 0 16 16"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z"
+                    />
+                  </svg>
                 </button>
               </div>
               <div className="map_story">
@@ -282,29 +287,60 @@ const Home = () => {
               </div>
               <div>
                 <button
-                  className="bb"
+                  className="bb set"
                   onClick={() => {
                     nextStory();
                   }}
                 >
-                  next
+                  <svg
+                    className="svg"
+                    width="20"
+                    height="20"
+                    viewBox="0 0 16 16"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-4.5-.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z"
+                    />
+                  </svg>
                 </button>
               </div>
             </div>
 
             <br />
             <div className="button_story">
-              <input
-                type="file"
-                onChange={(e) => {
-                  setImgStory(e.target.files[0]);
-                }}
-              ></input>
-              <button className="bb" onClick={uploadImage}>
-                Add
+              <label>
+                <svg width="16" height="16" viewBox="0 0 16 16">
+                  <path d="M6.002 5.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z" />
+                  <path d="M2.002 1a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V3a2 2 0 0 0-2-2h-12zm12 1a1 1 0 0 1 1 1v6.5l-3.777-1.947a.5.5 0 0 0-.577.093l-3.71 3.71-2.66-1.772a.5.5 0 0 0-.63.062L1.002 12V3a1 1 0 0 1 1-1h12z" />
+                </svg>
+                <input
+                  id="inputTag"
+                  className="choosePic"
+                  type="file"
+                  onChange={(e) => {
+                    setImgStory(e.target.files[0]);
+                  }}
+                ></input>
+              </label>
+              <button className="bb set" onClick={uploadImage}>
+                <svg
+                  width="16"
+                  height="16"
+                  
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M4.406 1.342A5.53 5.53 0 0 1 8 0c2.69 0 4.923 2 5.166 4.579C14.758 4.804 16 6.137 16 7.773 16 9.569 14.502 11 12.687 11H10a.5.5 0 0 1 0-1h2.688C13.979 10 15 8.988 15 7.773c0-1.216-1.02-2.228-2.313-2.228h-.5v-.5C12.188 2.825 10.328 1 8 1a4.53 4.53 0 0 0-2.941 1.1c-.757.652-1.153 1.438-1.153 2.055v.448l-.445.049C2.064 4.805 1 5.952 1 7.318 1 8.785 2.23 10 3.781 10H6a.5.5 0 0 1 0 1H3.781C1.708 11 0 9.366 0 7.318c0-1.763 1.266-3.223 2.942-3.593.143-.863.698-1.723 1.464-2.383z" />
+                  <path d="M7.646 4.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707V14.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708l3-3z" />
+                </svg>{" "}
               </button>
 
-              <button onClick={createStory}>Add Story </button>
+              <button className="bb set" onClick={createStory}>
+                <svg width="16" height="16" viewBox="0 0 16 16">
+                  <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+                  <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z" />
+                </svg>
+              </button>
             </div>
           </div>
           {/* ====================Post==================== */}
@@ -313,80 +349,85 @@ const Home = () => {
             <div className="main">
               {posts
                 ? posts.map((element, index) => {
+                  
                     return (
                       <div className="map_post" key={index}>
                         <div className="img_main">
                           <img id="img_post" src={element.media} />
                         </div>
                         <div className="icon_main">
-                          {/* ====like==== */}
-                          <button
-                            id="icon_home"
-                            onClick={() => {
-                              toLikes(element.id, element.likes);
-                            }}
-                          >
-                            <BsHeart id="icon" />
-                          </button>
-                          <button
-                            id="icon_home"
-                            onClick={() => {
-                              setShow(element.id);
-                              getCommentById(element.id);
-                            }}
-                          >
-                            <FaRegComment id="icon" />
-                          </button>
-
-                          <button
-                            style={{ display: !status_b ? "block" : "none" }}
-                            onClick={() => {
-                              element.id === showBook && status_b ? (
-                                <></>
-                              ) : (
-                                createBookmark(element.id)
-                              );
-                            }}
-                          >
-                            <svg
-                              xmlns="http://www.w3.org/2000/svg"
-                              width="16"
-                              height="16"
-                              fill="currentColor"
-                              className="bi bi-bookmark-fill"
-                              viewBox="0 0 16 16"
+                          <div className="headicon">
+                            <button
+                              id="icon_home"
+                              onClick={() => {
+                                toLikes(element.id, element.likes);
+                              }}
                             >
-                              <path d="M2 2v13.5a.5.5 0 0 0 .74.439L8 13.069l5.26 2.87A.5.5 0 0 0 14 15.5V2a2 2 0 0 0-2-2H4a2 2 0 0 0-2 2z" />
-                            </svg>{" "}
-                          </button>
-                          <button
-                            style={{ display: status_b ? "block" : "none" }}
-                            onClick={() => {
-                              element.id === removeBook && status_b ? (
-                                removeBookmark(element.id)
-                              ) : (
-                                <></>
-                              );
-                            }}
-                          >
-                            <svg
-                              xmlns="http://www.w3.org/2000/svg"
-                              width="16"
-                              height="16"
-                              fill="currentColor"
-                              className="bi bi-bookmark"
-                              viewBox="0 0 16 16"
+                              <BsHeart id="icon" />
+                            </button>
+                            <button
+                              id="icon_home"
+                              onClick={() => {
+                                setShow(element.id);
+                                getCommentById(element.id);
+                              }}
                             >
-                              <path d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v13.5a.5.5 0 0 1-.777.416L8 13.101l-5.223 2.815A.5.5 0 0 1 2 15.5V2zm2-1a1 1 0 0 0-1 1v12.566l4.723-2.482a.5.5 0 0 1 .554 0L13 14.566V2a1 1 0 0 0-1-1H4z" />
-                            </svg>
-                          </button>
+                              <FaRegComment id="icon" />
+                            </button>
 
-                          <h4>{element.likes} Likes</h4>
-
-                          <p id="p_post">{element.description}</p>
-                          <br />
+                            <button
+                              className="bookmarkmark"
+                              style={{ display: !status_b ? "block" : "none" }}
+                              onClick={() => {
+                                element.id === showBook && status_b ? (
+                                  <></>
+                                ) : (
+                                  createBookmark(element.id)
+                                );
+                              }}
+                            >
+                              <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="16"
+                                height="16"
+                                
+                                className="bi bi-bookmark-fill"
+                                viewBox="0 0 16 16"
+                              >
+                                <path d="M2 2v13.5a.5.5 0 0 0 .74.439L8 13.069l5.26 2.87A.5.5 0 0 0 14 15.5V2a2 2 0 0 0-2-2H4a2 2 0 0 0-2 2z" />
+                              </svg>{" "}
+                            </button>
+                            <button
+                              className="bookmarkmark"
+                              style={{ display: status_b ? "block" : "none" }}
+                              onClick={() => {
+                                element.id === removeBook && status_b ? (
+                                  removeBookmark(element.id)
+                                ) : (
+                                  <></>
+                                );
+                              }}
+                            >
+                              <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="16"
+                                height="16"
+                               
+                                className="bi bi-bookmark"
+                                viewBox="0 0 16 16"
+                              >
+                                <path d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v13.5a.5.5 0 0 1-.777.416L8 13.101l-5.223 2.815A.5.5 0 0 1 2 15.5V2zm2-1a1 1 0 0 0-1 1v12.566l4.723-2.482a.5.5 0 0 1 .554 0L13 14.566V2a1 1 0 0 0-1-1H4z" />
+                              </svg>
+                            </button>
+                          </div>
+                          <div className="numLikes">
+                            <h4>{element.likes} Likes</h4>
+                          </div>
+                          <p id="p_post"><h4>{element.id}:</h4> <p className="desc">{element.description}</p></p>
+                          <div className="datee">
                           <p>{element.date}</p>
-
+                          </div>
+                          <div className="footerpost"><input className="azx" placeholder="Add a comment.."></input><p className="cruser">post</p></div>
                           {/* ========== comment ======== */}
 
                           <div
