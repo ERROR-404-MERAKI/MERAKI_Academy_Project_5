eca208760096ae3a4154502ab82b8b4c50747585
MousaHussen <Mousahusseen@gmail.com>
2022-06-11 13:20:15 +0300

    after editing

--- frontend/src/components/Home/index.js
+++ frontend/src/components/Home/index.js
@@ -158,7 +158,6 @@ const Home = () => {
     })
       .then((resp) => resp.json())
       .then((data) => {
-        console.log(data);
         setStory(data.url);
       })
       .catch((err) => console.log(err));
@@ -247,9 +246,7 @@ const Home = () => {
         <Navbar />
       </div>
       <div className="post_home">
-        <div className="left_side">
-          
-        </div>
+        <div className="left_side"></div>
         <div className="middle_side">
           <div className="storys-department">
             <div className="main_story">
@@ -324,12 +321,7 @@ const Home = () => {
                 ></input>
               </label>
               <button className="bb set" onClick={uploadImage}>
-                <svg
-                  width="16"
-                  height="16"
-                  
-                  viewBox="0 0 16 16"
-                >
+                <svg width="16" height="16" viewBox="0 0 16 16">
                   <path d="M4.406 1.342A5.53 5.53 0 0 1 8 0c2.69 0 4.923 2 5.166 4.579C14.758 4.804 16 6.137 16 7.773 16 9.569 14.502 11 12.687 11H10a.5.5 0 0 1 0-1h2.688C13.979 10 15 8.988 15 7.773c0-1.216-1.02-2.228-2.313-2.228h-.5v-.5C12.188 2.825 10.328 1 8 1a4.53 4.53 0 0 0-2.941 1.1c-.757.652-1.153 1.438-1.153 2.055v.448l-.445.049C2.064 4.805 1 5.952 1 7.318 1 8.785 2.23 10 3.781 10H6a.5.5 0 0 1 0 1H3.781C1.708 11 0 9.366 0 7.318c0-1.763 1.266-3.223 2.942-3.593.143-.863.698-1.723 1.464-2.383z" />
                   <path d="M7.646 4.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707V14.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708l3-3z" />
                 </svg>{" "}
@@ -351,7 +343,13 @@ const Home = () => {
                 ? posts.map((element, index) => {
                     return (
                       <div className="map_post" key={index}>
-                        <div className="headerName"><img className="imgpoUs" src={element.ProfilePicture} /> {element.firstName} {element.lastName}</div>
+                        <div className="headerName">
+                          <img
+                            className="imgpoUs"
+                            src={element.ProfilePicture}
+                          />{" "}
+                          {element.firstName} {element.lastName}
+                        </div>
                         <div className="img_main">
                           <img id="img_post" src={element.media} />
                         </div>
@@ -390,7 +388,6 @@ const Home = () => {
                                 xmlns="http://www.w3.org/2000/svg"
                                 width="16"
                                 height="16"
-                                
                                 className="bi bi-bookmark-fill"
                                 viewBox="0 0 16 16"
                               >
@@ -412,7 +409,6 @@ const Home = () => {
                                 xmlns="http://www.w3.org/2000/svg"
                                 width="16"
                                 height="16"
-                               
                                 className="bi bi-bookmark"
                                 viewBox="0 0 16 16"
                               >
@@ -423,11 +419,22 @@ const Home = () => {
                           <div className="numLikes">
                             <h4>{element.likes} Likes</h4>
                           </div>
-                          <p id="p_post"><h5>{element.firstName} {element.lastName}:</h5> <p className="desc">{element.description}</p></p>
+                          <p id="p_post">
+                            <h5>
+                              {element.firstName} {element.lastName}:
+                            </h5>{" "}
+                            <p className="desc">{element.description}</p>
+                          </p>
                           <div className="datee">
-                          <p>{element.date}</p>
+                            <p>{element.date}</p>
                           </div>
-                          <div className="footerpost"><input className="azx" placeholder="Add a comment.."></input><p className="cruser">post</p></div>
+                          <div className="footerpost">
+                            <input
+                              className="azx"
+                              placeholder="Add a comment.."
+                            ></input>
+                            <p className="cruser">post</p>
+                          </div>
                           {/* ========== comment ======== */}
 
                           <div
