0095d0ea6a17fcdedc66bbe55e07f2f5bde14cf3
HasanTorman <torman.hassan@gmail.com>
2022-06-16 21:39:23 +0300

    update

--- backend/routes/room.js
+++ backend/routes/room.js
@@ -1,12 +1,12 @@
 const express = require("express");
 
-const {createRoom}  = require("../controllers/room");
+const { createRoom, getRoom } = require("../controllers/room");
 const { authentication } = require("../middlewares/authentication");
-
 
 const roomRouter = express.Router();
 
+roomRouter.post("/:id", authentication, createRoom);
 
-roomRouter.post("/:id",authentication, createRoom);
+roomRouter.get("/:id", authentication, getRoom);
 
 module.exports = roomRouter;
