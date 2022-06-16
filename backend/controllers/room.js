2c140686b38b4b8b7f7fcb1c4d42eaaef7a80d4d
HasanTorman <torman.hassan@gmail.com>
2022-06-16 21:39:11 +0300

    update

--- backend/controllers/room.js
+++ backend/controllers/room.js
@@ -39,6 +39,31 @@ const createRoom = (req, res) => {
   });
 };
 
+const getRoom = (req, res) => {
+  const resive = req.params.id;
+  const sender = req.token.userId;
+  const query = `SELECT * FROM room WHERE resive = ? AND sender = ? `;
+  const data = [resive, sender];
+  connection.query(query, data, (err, result) => {
+    console.log(data);
+    if (err) {
+      return res.status(500).json({
+        success: false,
+        massage: "server error",
+        err: err,
+      });
+    }
+    if (result.length) {
+      res.status(201).json({
+        success: true,
+        massage: "room exiest",
+        result,
+      });
+    }
+  });
+};
+
 module.exports = {
   createRoom,
+  getRoom,
 };
